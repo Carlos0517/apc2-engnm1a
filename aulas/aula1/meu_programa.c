1beec23356b6aec2cdb6383e89670d96120857fe
Carlos0517 <98491563+Carlos0517@users.noreply.github.com>
2024-03-02 13:30:17 +0000

    Meu programa funcionando sem Wagrnings

--- aulas/aula1/meu_programa.c
+++ aulas/aula1/meu_programa.c
@@ -2,23 +2,27 @@
 
 int main() {
   
-float nota1 = 0.0;
-float nota2 = 0.0; 
+    float nota1 = 0.0;
+    float nota2 = 0.0; 
+    int tamanho = 0;
+  
+    printf(" entre com a nota1:");
+    tamanho = scanf("%f", &nota1 );
+    
+    printf(" entre com a nota2: ");
+    tamanho = scanf("%f", &nota2 );
 
-printf(" entre com a nota1: ");
-scanf(" %f", &nota1);
-
-printf(" entre com a nota2: ");
-scanf(" %f", &nota2 );
-
-float media = 0.4*nota1 + 0.6*nota2;
-printf(" a media final é %.1f g", media);
-
-if(media >= 5.0 ) {
-  printf("passei!\n" );
-}
-else {
-  printf("não passei!\n");
-}  
+   
+    float media = 0.4*nota1 + 0.6*nota2;
+    printf(" a media final é %.1f", media);
+    
+    if(media >= 6.0 ) {
+        printf("Passei!\n" );
+    }
+    else {
+        printf("Não passei!\n");
+    }  
+    
   return 0;
-  } 
+  
+}
